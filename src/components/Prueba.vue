72edbf320051595c2e5505d69650b826acac75ed
RenHec <31858903+RenHec@users.noreply.github.com>
2021-02-01 22:02:18 -0600

    inicio del respositorio

---
+++ src/components/Prueba.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row justify="center" align="center">
+      Hola
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Prueba",
+  components: {
+  },
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
---
+++ src/services/seguridad/rol/RolService.js
@@ -0,0 +1,21 @@
+class RolService {
+  axios
+  baseUrl
+
+  constructor(axios, baseUrl) {
+    this.axios = axios
+    this.baseUrl = baseUrl + 'service/rest/v1/security/rol'
+  }
+
+  store(data) {
+    let self = this
+    return self.axios.post(`${self.baseUrl}`, data)
+  }
+
+  destroy(data) {
+    let self = this;
+    return self.axios.delete(`${self.baseUrl}/${data.id}`);
+  }
+}
+
+export default RolService
